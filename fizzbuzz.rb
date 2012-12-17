91268d41b91d373df250ed4acf4d8a558ec5bae9
Sean <sdoesimkins@gmail.com>
2012-12-17 15:00:28 -0600

    support command line entry of number range and divisors

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
-def fizz_buzz(first_divisor, second_divisor)
-	100.times do |i|
+def fizz_buzz(range, first_divisor, second_divisor)
+	range.times do |i|
 		if (i + 1) % first_divisor == 0 && (i + 1) % second_divisor == 0
 			puts "fizzbuzz"
 		elsif (i + 1) % first_divisor == 0
@@ -13,4 +13,8 @@ def fizz_buzz(first_divisor, second_divisor)
 	end
 end
 
-fizz_buzz(7, 9)
+range = ARGV[0].to_i
+first_divisor = ARGV[1].to_i
+second_divisor = ARGV[2].to_i
+
+fizz_buzz(range, first_divisor, second_divisor)
