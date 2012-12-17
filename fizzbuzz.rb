9cf76c94ed91fc3c1875fa2d9ad886847bf2e22a
Sean <sdoesimkins@gmail.com>
2012-12-17 14:47:23 -0600

    variable fizz & buzz nums

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
-def fizz_buzz
+def fizz_buzz(first_divisor, second_divisor)
 	100.times do |i|
-		if (i + 1) % 3 == 0 && (i + 1) % 5 == 0
+		if (i + 1) % first_divisor == 0 && (i + 1) % second_divisor == 0
 			puts "fizzbuzz"
-		elsif (i + 1) % 3 == 0
+		elsif (i + 1) % first_divisor == 0
 			puts "fizz"
-		elsif (i + 1) % 5 == 0
+		elsif (i + 1) % second_divisor == 0
 			puts "buzz"
 		else
 			puts i+1
@@ -13,4 +13,4 @@ def fizz_buzz
 	end
 end
 
-fizz_buzz
+fizz_buzz(7, 9)
